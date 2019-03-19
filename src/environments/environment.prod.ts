9a6d25e456f773b41cf6a18c880856e44b0b6459
AllanHosokawa <7543284+AllanHosokawa@users.noreply.github.com>
2019-03-19 14:08:21 -0300

    Creating a New Project

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,11 @@
+export const environment = {
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyChBERzFU0QKlm7k17byhgEaOOZHiMU6iw",
+    authDomain: "organic-shop-81a1b.firebaseapp.com",
+    databaseURL: "https://organic-shop-81a1b.firebaseio.com",
+    projectId: "organic-shop-81a1b",
+    storageBucket: "organic-shop-81a1b.appspot.com",
+    messagingSenderId: "228840172424"
+  }
+};
