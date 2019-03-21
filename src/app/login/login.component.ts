f0d692b44253c9ce2e24d6b5b13ff1cfc26144a3
AllanHosokawa <7543284+AllanHosokawa@users.noreply.github.com>
2019-03-21 10:59:45 -0300

    Extracting a Service

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,6 +1,5 @@
-import { Component, OnInit } from '@angular/core';
-import { AngularFireAuth } from '@angular/fire/auth';
-import { auth } from 'firebase/app';
+import { Component } from '@angular/core';
+import { AuthService } from '../auth.service';
 
 @Component({
   selector: 'app-login',
@@ -9,9 +8,9 @@ import { auth } from 'firebase/app';
 })
 export class LoginComponent {
 
-  constructor(private afAuth: AngularFireAuth) { }
+  constructor(private auth: AuthService) { }
 
   login() {
-    this.afAuth.auth.signInWithRedirect(new auth.GoogleAuthProvider());
+    this.auth.login();
   }
 }
