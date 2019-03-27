7822e8232b3c88eb66ce119edc1a2c58a747c931
AllanHosokawa <7543284+AllanHosokawa@users.noreply.github.com>
2019-03-27 09:58:30 -0300

    Extracting an Interface

--- src/app/product.service.ts
+++ src/app/product.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import { AngularFireDatabase } from '@angular/fire/database';
 import { map } from 'rxjs/operators';
+import { Product } from './models/product';
 
 @Injectable({
   providedIn: 'root'
@@ -14,7 +15,7 @@ export class ProductService {
   }
 
   getAll() {
-    return this.db.list('/products')
+    return this.db.list<Product>('/products')
     .snapshotChanges().pipe(
       map(actions =>
         actions.map(a => ({ key: a.key, ...a.payload.val() }))
