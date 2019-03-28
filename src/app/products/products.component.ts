ebd13f45ab424dd33fae83849ea7f1c1c04017e5
AllanHosokawa <7543284+AllanHosokawa@users.noreply.github.com>
2019-03-28 11:08:26 -0300

    Refactoring: Extracting ProductFilterComponent

--- src/app/products/products.component.ts
+++ src/app/products/products.component.ts
@@ -1,6 +1,5 @@
 import { Component } from '@angular/core';
 import { ProductService } from '../product.service';
-import { CategoryService } from '../category.service';
 import { ActivatedRoute } from '@angular/router';
 import { Product } from '../models/product';
 import { switchMap } from 'rxjs/operators';
@@ -13,11 +12,9 @@ import { switchMap } from 'rxjs/operators';
 export class ProductsComponent {
   products: Product[] = [];
   filteredProducts: Product[] = [];
-  categories$;
   category: string;
 
-  constructor(activatedRoute: ActivatedRoute, productService: ProductService,
-    categoryService: CategoryService) {
+  constructor(activatedRoute: ActivatedRoute, productService: ProductService) {
     productService.getAll()
       .pipe(switchMap(products => {
         this.products = products;
@@ -30,7 +27,5 @@ export class ProductsComponent {
           this.products.filter(p => p.category === this.category) :
           this.products;
       });
-
-    this.categories$ = categoryService.getAll();
   }
 }
